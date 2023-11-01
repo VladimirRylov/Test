9b10632c0ab939398012aaf6045eb6b672e34b9d
Владимир Рылов <55760201+VladimirRylov@users.noreply.github.com>
2023-11-01 23:53:11 +0200

    Update fake.ts

--- Folder/Folder_2/fake.ts
+++ Folder/Folder_2/fake.ts
@@ -1,5 +1,5 @@
 class SomeClass{
-    
+
     private add(tables: IDatabaseTables, itemTpl: string, name: string, shortName: string, Description: string)
     {
         // For each language, add locale for the new trader
@@ -12,8 +12,19 @@ class SomeClass{
         }
     }
     
-    class Some{
-      
+    
+}
+interface Isome{
+    private addss(tables: IDatabaseTables, itemTpl: string, name: string, shortName: string, Description: string)
+    {
+        // For each language, add locale for the new trader
+        console.log(`USEC_COMMANDER: For each language, adding item locale for the new trader `);
+        const locales = Object.values(tables.locales.global) as Record<string, string>[];
+        for (const locale of locales) {
+            locale[`${itemTpl} Name`] = name;
+            locale[`${itemTpl} ShortName`] = shortName;
+            locale[`${itemTpl} Description`] = Description;
+        }
     }
 }
 
