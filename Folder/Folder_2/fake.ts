2eb86de1ac597950fc96e29e5bfb884345d688c4
Владимир Рылов <55760201+VladimirRylov@users.noreply.github.com>
2023-11-01 23:30:55 +0200

    Update fake.ts

--- Folder/Folder_2/fake.ts
+++ Folder/Folder_2/fake.ts
@@ -10,4 +10,59 @@ private addItemToLocales(tables: IDatabaseTables, itemTpl: string, name: string,
             locale[`${itemTpl} Description`] = Description;
         }
     }
+    public preAkiLoad(container: DependencyContainer): void 
+    {
+        try 
+        {
+            const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
+
+            staticRouterModService.registerStaticRouter(
+                `${PunisherBossMod.modName}:ProfileSelected`,
+                [
+                    {
+                        url: "/client/game/profile/select",
+                        action: (url, info, sessionId, output) => 
+                        {
+                            PunisherBossMod.sessionID = sessionId;
+                            return output;
+                        }
+                    }
+                ],
+                "aki"
+            );
+
+            // Raid Saving (End of raid)
+            staticRouterModService.registerStaticRouter(
+                `${PunisherBossMod.modName}:RaidSaved`,
+                [
+                    {
+                        url: "/raid/profile/save",
+                        action: (url, info, sessionId, output) => 
+                        {
+                            console.log("PUNISHER: Info:", info);
+
+                            if (info && info.profile && info.profile.Stats && info.profile.Stats.Victims && info.profile.Stats.Aggressor) 
+                            {
+                               console.log("PUNISHER: Victims:", info.profile.Stats.Victims);
+                               console.log("PUNISHER: Aggressor:", info.profile.Stats.Aggressor);
+                            }
+                            else 
+                            {
+                               console.log("PUNISHER: Victims or Aggressor array (table info) is not present in the profile.");
+                            }
+                            PunisherBossMod.removeBossSpawnFromMaps();
+                            PunisherBossMod.onRaidSave(url, info, sessionId, output);
+                            PunisherBossMod.setBossChanceFromProgressFile();
+                            return output;
+                        }
+                    }
+                ],
+                "aki"
+            );
+        }
+        catch (error) 
+        {
+            this.logger.error(error);
+        }
+    }
 }
